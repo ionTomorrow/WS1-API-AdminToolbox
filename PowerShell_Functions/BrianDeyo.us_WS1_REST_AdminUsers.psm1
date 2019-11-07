f8ff158d2416b9e6457b6cbb3c8dbad3f85f3344
briandeyo <briandeyo@users.noreply.github.com>
2019-11-07 09:23:36 -0500

    Added set-ws1AdminUser

--- PowerShell_Functions/BrianDeyo.us_WS1_REST_AdminUsers.psm1
+++ PowerShell_Functions/BrianDeyo.us_WS1_REST_AdminUsers.psm1
@@ -62,11 +62,84 @@ Function New-WS1AdminUser {
         if ($TimeZone -ne $null) {$body.Add("TimeZone", $TimeZone)}
         if ($LocationGroupId -ne $null) {$body.Add("LocationGroupId", $LocationGroupId)}
         if ($Locale -ne $null) {$body.Add("Locale", $Locale)}
-        if ($Roles -ne $null) {$body.Add("Roles", (ConvertTo-Json $Roles))}
+        if ($Roles -ne $null) {$body.Add("Roles",$roles)}
         if ($MessageType -ne $null) {$body.Add("MessageType", $MessageType)}
         if ($MessageTemplateId -ne $null) {$body.Add("MessageTemplateId", $MessageTemplateId)}
     }
 
     $ws1AdminAdd = Invoke-Restmethod -Method POST -Uri https://$WS1EnvUri/api/system/admins/addadminuser -Body (ConvertTo-Json $body) -Headers $Headers
     return $ws1AdminAdd
+}
+
+Function Set-ws1AdminUser {
+
+    <#
+        .SYNOPSIS
+            Create a new BASIC or DIRECTORY Admin User
+        .DESCRIPTION
+            Creates a new Admin user account.
+        .EXAMPLE
+            Set-WS1AdminUser -
+        .PARAMETER ws1adminId
+        .PARAMETER IsActiveDirectoryUser
+        .PARAMETER UserName
+        .PARAMETER Password
+        .PARAMETER FirstName
+        .PARAMETER LastName
+        .PARAMETER Status
+        .PARAMETER Email        
+        .PARAMETER TimeZone
+        .PARAMETER LocationGroupId
+        .PARAMETER Locale
+        .PARAMETER InitialLandingPage
+        .PARAMETER Roles
+        .PARAMETER RoleID
+        .PARAMETER LocationGroupId
+        .PARAMETER RequiresPasswordChange
+
+  #>
+    
+  param (
+    
+    [Parameter(Mandatory=$true, Position=0)][int]$ws1adminId,
+    [Parameter(Mandatory=$false, Position=1)][string]$UserName,
+    [Parameter(Mandatory=$false, Position=2)][string]$Password,
+    [Parameter(Mandatory=$false, Position=3)][string]$FirstName,
+    [Parameter(Mandatory=$false, Position=4)][string]$LastName,
+    [Parameter(Mandatory=$false, Position=5)][string]$Email,
+    [Parameter(Mandatory=$false, Position=6)][bool]$IsActiveDirectoryUser, 
+    [Parameter(Mandatory=$false, Position=7)][string]$TimeZone,
+    [Parameter(Mandatory=$false, Position=8)][int]$LocationGroupId,
+    [Parameter(Mandatory=$false, Position=9)][string]$Locale,
+    [Parameter(Mandatory=$false, Position=10)][string]$InitialLandingPage,      
+    [Parameter(Mandatory=$false, Position=11)][array]$Roles,
+    [Parameter(Mandatory=$false, Position=12)][bool]$RequiresPasswordChange,
+    [Parameter(Mandatory=$false, Position=13)][string]$MessageType,
+    [Parameter(Mandatory=$false, Position=14)][int]$MessageTemplateId,
+    [Parameter(Mandatory=$true, Position=15,ValueFromPipelineByPropertyName=$true)][Hashtable]$headers
+    )
+
+    $ws1Envuri = $headers.ws1ApiUri
+    ### Creation of JSON payload
+    $body = @{}
+    $body.Add("IsActiveDirectoryUser", $IsActiveDirectoryUser)
+
+
+    if ($IsActiveDirectoryUser -eq $false) {
+        if ($UserName -ne $null) {$body.Add("UserName", $UserName)}
+        if ($Password -ne $null) {$body.Add("Password", $Password)}
+        if ($FirstName -ne $null) {$body.Add("FirstName", $FirstName)}
+        if ($LastName -ne $null) {$body.Add("LastName", $LastName)}
+        if ($Email -ne $null) {$body.Add("Email", $Email)}        
+        if ($LocationGroupId -ne $null) {$body.Add("LocationGroupId", $LocationGroupId)}
+        if ($TimeZone -ne $null) {$body.Add("TimeZone", $TimeZone)}
+        if ($Locale -ne $null) {$body.Add("Locale", $Locale)}
+        if ($InitialLandingPage -ne $null) {$body.Add("InitialLandingPage", $InitialLandingPage)}
+        if ($Roles -ne $null) {$body.Add("Roles",$roles)}
+        if ($MessageType -ne $null) {$body.Add("MessageType", $MessageType)}
+        if ($MessageTemplateId -ne $null) {$body.Add("MessageTemplateId", $MessageTemplateId)}
+    }
+    $ws1AdminUpdate = Invoke-RestMethod -Method POST https://host/api/system/admins/$ws1adminId/update -Body (ConvertTo-Json $body) -Headers $headers
+
+    return $ws1AdminUpdate
 }
