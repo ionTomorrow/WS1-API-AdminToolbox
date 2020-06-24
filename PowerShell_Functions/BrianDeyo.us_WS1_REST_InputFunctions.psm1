0949150008439fd3a5c7f67f038d0709bde3054f
Brian Richard Deyo (CENSUS/DCEO CTR) <brian.r.deyo@census.gov>
2020-06-24 16:48:29 -0400

    Update BrianDeyo.us_WS1_REST_InputFunctions.psm1
    Included new import-ws1csv function

--- PowerShell_Functions/BrianDeyo.us_WS1_REST_InputFunctions.psm1
+++ PowerShell_Functions/BrianDeyo.us_WS1_REST_InputFunctions.psm1
@@ -18,7 +18,7 @@ Function Import-WS1DeviceCsv {
     <#Retrieve CSV from input directory #>
     write-host `n
     $inputPath = ((get-location).path)+"\input\"
-    Write-host -ForegroundColor Cyan "The expectation is that the .csv file with Serial Numbers is found in the $inputPath Scripting folder."
+    #Write-host -ForegroundColor Cyan "The expectation is that the .csv file with Serial Numbers is found in the $inputPath Scripting folder."
     $inputList = @()
     $inputCsv = $null
     
@@ -33,10 +33,15 @@ Function Import-WS1DeviceCsv {
             }
             
             $validCsv = Test-Path -Path $inputpath$inputCsv".csv"
-            $verifyCsv = get-item -path $inputpath$inputCsv".csv" 
+            $verifyCsv = get-item -path $inputpath$inputCsv".csv"
             $verifyCsvFileName = $verifyCsv.name   
             $hashCsv = ($verifyCsv | Get-FileHash -Algorithm SHA256).hash
             $inputList = import-csv $inputPath$verifyCsvFilename
+            
+            <#if (!$ColumnName) {
+                $inputList
+                [string]$columnName = Read-Host "what is the column name?"
+            }#>
         }
         Catch {
             write-host -ForegroundColor Red "Error Importing filename"
@@ -48,6 +53,106 @@ Function Import-WS1DeviceCsv {
     ###return imported CSV as an object and the filehash if requested by script.
     ###Multiple values can be retrieved by accessing results like an array - https://social.technet.microsoft.com/Forums/ie/en-US/65d3bf7f-c710-498a-b535-46c64cbf92e7/return-multiple-values-in-powershell?forum=ITCG
     Return $inputList
+#    Return $ColumnName
+    if ($GetFileHash) {
+        Return $hashCsv
+    }
+
+}
+
+Function Import-WS1Csv {
+    param(
+        [Parameter(Mandatory=$true,Position=0)][string]$defaultFilename,
+        [Parameter(Mandatory=$false,Position=1)][bool]$GetFileHash,
+        [Parameter(Mandatory=$false,Position=2)][string]$uniqueHeader
+    )
+
+    <#Retrieve CSV from input directory #>
+    $inputPath = ((get-location).path)+"\input\"
+    if ((test-path $inputPath) -eq $false) {
+        $errorMsg = "[ERROR] $inputPath is an invalid location!"
+        ###This should probably prompt for someone to actually type in the correct path...
+        return $errorMsg
+        exit
+    }
+
+    $inputList = @()
+    $inputCsv = $null
+      
+    
+    Do {
+    $inputCsv = Read-Host -Prompt "please input name of the csv (with or without .csv extension). Press enter to use default file name ($defaultFilename)"
+    
+        Try {
+            if (!$inputCsv) {
+                $inputCsv = $defaultFilename
+            }
+            
+            ###Pull last four characters of input file name. This will allow people to include or exclude the .csv file extension
+            ###Included from examples at https://ss64.com/ps/right.html
+            $startchar = [math]::min($inputCsv.length - 4,$inputCsv.length)
+            $startchar = [math]::max(0, $startchar)
+            $length = [math]::min($inputCsv.length, 4)
+            if ($inputCsv.SubString($startchar ,$length) -ne ".csv") {
+                $inputCsv=$inputCsv+".csv"
+            }
+
+            $verifyCsv = get-item -path ($inputpath+$inputCsv)
+
+            $hashCsv = ($verifyCsv | Get-FileHash -Algorithm SHA256).hash
+            [System.Collections.ArrayList]$headerRow = @()
+
+            ###obtain header row for .csv
+            [System.Collections.ArrayList]$headerRow = (Get-Content $verifyCsv | Select-Object -First 1).Split(",")
+            
+            ###Validate that the Unique Identifier presented in the function call or found in the input is valid
+            Do {
+                
+                ###On first run check if uniqueHeader parameter was passed in function call. If not parse header for possible options to choose from
+                if (!$uniqueHeader) {
+
+                    if (!$validHeader) {
+                        $i=0
+                        for ($i=0;$i -lt $headerRow.count;$i++) {
+                            write-host "[$i] - $($headerRow[$i])"
+                            
+        
+                        }
+                    }
+                    [int]$uniqueColumn = Read-Host -Prompt "Type Number of the Unique Identifier for the input file or push enter to repreat the list of imported headers."
+                    [string]$uniqueHeader = $headerRow[$uniqueColumn]
+                }
+                
+                if ($headerRow -contains $uniqueHeader){
+                    [bool]$validHeader = $true
+                }
+                else {
+                    write-host -ForegroundColor Yellow "    WARNING: Unique Header not found with specified value. Please try again!"
+                    remove-variable uniqueHeader
+                    
+                }
+                
+            }
+            Until ($validHeader)
+
+            $inputList = import-csv $verifyCsv
+            if ($inputList) {
+                $validCsv = $true
+            }
+
+
+        }
+        Catch {
+            write-host -ForegroundColor Red "[ERROR] Unable to Import filename $verifyCsv. Please try again"
+            $validCsv = $false
+        }
+    }
+    Until ($validCsv)
+
+    ###return imported CSV as an object and the filehash if requested by script.
+    ###Multiple values can be retrieved by accessing results like an array - https://social.technet.microsoft.com/Forums/ie/en-US/65d3bf7f-c710-498a-b535-46c64cbf92e7/return-multiple-values-in-powershell?forum=ITCG
+    Return $inputList, $hashCsv, $uniqueHeader
+#    Return $ColumnName
     if ($GetFileHash) {
         Return $hashCsv
     }
@@ -80,7 +185,7 @@ function get-threadCount {
     #>
 
     ###Check if Windows. This should probably be reversed since *most* people will run PowerShell Scripts on Windows
-    if ($PSVersionTable.Platform -eq "Windows") {
+    if ($PSVersionTable.PSEdition -eq "Desktop") {
         $psVer = ($PSVersionTable.PSVersion).Major
         if ($psVer -gt 5) {
             [int]$threads = ((Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors -2)
