9c95bbe929419654876c8028234d3a1ff34bd35b
deyo0001 <brian.r.deyo@census.gov>
2020-11-11 15:45:28 -0500

    Updated Logging Functions
    new-ws1InputArchive function will build an input archive tree similar to output based on yyyy\mm\dd
    
    Also moved logging functions from the ConnectionConfig to the InputFunctions module

--- PowerShell_Functions/BrianDeyo.us_WS1_REST_InputFunctions.psm1
+++ PowerShell_Functions/BrianDeyo.us_WS1_REST_InputFunctions.psm1
@@ -64,23 +64,31 @@ Function Import-WS1Csv {
     <#.SYNOPSIS
     Imports a .csv and obtains specific characteristics of the .csv useable for WS1 scripts and functions
     .DESCRIPTION
-    Imports a .csv and identifies the column of the .csv that includes the unique identifier for each row. Optionally a filehash can be produced for comparison against an existed .csv.
+    Imports a .csv and identifies the column of the .csv that includes the unique identifier for each row.
+    The .csv can be specified using the inputCsv parameter. If that parameter is left blank the function will prompt for a filename.
+    Optionally a filehash can be produced for comparison against an existed .csv.
 
     CHANGELOG
     2020-07-09 - modified header retrieval to strip off quote marks ".
+    2020-09-22 - Added the inputCsv option as a way to automate ingestion of a file and suppress prompting for the filename on the first attempt
+                    This should really be changed to remove the defaultFilename parameter completely, or at least switch it to false. 
+                        It has been left primarily for backwards compatibility with existing scripts.
+
     
     .EXAMPLE
-    import-ws1Csv -defaultFileName "Device Inventory" -getFileHash $true -uniqueHeader "Serial Number"
+    import-ws1Csv -defaultFileName "Device Inventory" -getFileHash $true -uniqueHeader "Serial Number" -inputCsv input.csv
     .PARAMETER defaultFileName
     .PARAMETER getFileHash
     .PARAMETER uniqueHeader
+    .PARAMETER inputCsv
     
 
     #>
     param(
         [Parameter(Mandatory=$true,Position=0)][string]$defaultFilename,
         [Parameter(Mandatory=$false,Position=1)][bool]$GetFileHash,
-        [Parameter(Mandatory=$false,Position=2)][string]$uniqueHeader
+        [Parameter(Mandatory=$false,Position=2)][string]$uniqueHeader,
+        [Parameter(Mandatory=$false,Position=3)][string]$inputCsv
     )
 
     <#Retrieve CSV from input directory #>
@@ -93,16 +101,22 @@ Function Import-WS1Csv {
     }
 
     $inputList = @()
-    $inputCsv = $null
+    
       
     
     Do {
-    $inputCsv = Read-Host -Prompt "please input name of the csv (with or without .csv extension). Press enter to use default file name ($defaultFilename)"
+    
     
         Try {
-            if (!$inputCsv) {
-                $inputCsv = $defaultFilename
+            do {
+                if (!$inputCsv) {
+                    $inputCsv = Read-Host -Prompt "Please input name of the csv (with or without .csv extension). Press enter to use default file name ($defaultFilename)"
+                    if (!$inputCsv) {
+                        $inputCsv = $defaultFilename
+                    }
+                }
             }
+            until ($inputCsv)
             
             ###Pull last four characters of input file name. This will allow people to include or exclude the .csv file extension
             ###Included from examples at https://ss64.com/ps/right.html
@@ -180,8 +194,9 @@ Function Import-WS1Csv {
 
         }
         Catch {
-            write-host -ForegroundColor Red "[ERROR] Unable to Import filename $verifyCsv. Please try again"
+            write-host -ForegroundColor Red "[ERROR] Unable to Import filename $($inputCsv). Please try again"
             $validCsv = $false
+            remove-variable inputCsv
         }
     }
     Until ($validCsv)
@@ -246,4 +261,152 @@ function get-threadCount {
         $threads = 1
     }
     return $threads
+}
+
+
+<###############################
+###
+###  LOGGING FUNCTIONS
+###
+###############################>
+
+Function get-timestamp() {
+    $WS1LogTime = Get-Date -Format yyyyMMdd.HH.mm.ss
+    return $WS1LogTime
+}
+
+<#
+Function Update-WS1Log{
+param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateSet("PreCheck","Error","PostCheck")]
+        [string]$logType,
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateSet("iOS","Windows")]
+        [string]$platform
+       <# UnitID
+        ErrorCode
+        ErrorMessage
+        ErrorActivityID
+        
+
+        )
+
+}
+#>
+
+
+###Logging Format Setup
+###
+### Took hints from https://stackoverflow.com/questions/31982926/new-item-changes-function-return-value to use the | Out-NULL to remove extra paths with New-Item
+
+Function get-ws1LogFolder {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$ws1EnvUri
+        )
+    $logYear = Get-Date -Format yyyy
+    $logMonth = Get-Date -Format MM
+    $logDate = Get-Date -Format dd
+
+    
+    If ((Test-Path -path output\$logYear) -eq $false) {
+        New-Item -Path output\$logYear -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path output\$logYear\$logMonth) -eq $false) {
+        New-Item -Path output\$logYear\$logMonth -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path output\$logYear\$logMonth\$logDate) -eq $false) {
+        New-Item -Path output\$logYear\$logMonth\$logDate -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path output\$logYear\$logMonth\$logDate\$ws1EnvUri) -eq $false) {
+        New-Item -Path output\$logYear\$logMonth\$logDate\$ws1EnvUri -ItemType Directory | Out-Null
+    }
+    
+    $ws1LogPath = (Get-Item output\$logYear\$logMonth\$logDate\$ws1EnvUri).FullName
+    return  $ws1LogPath;
+}
+
+
+
+Function get-ws1InputArchive {
+    
+    $currentUser = [Environment]::UserName
+    $logYear = Get-Date -Format yyyy
+    $logMonth = Get-Date -Format MM
+    $logDate = Get-Date -Format dd
+
+    $inputPath = ((get-location).path)+"\input"
+    $archivePath = ((get-location).path)+"\input\archive"
+
+    if (!(Test-path -path ($inputPath+"\archive"))) {
+        New-Item -path ($inputPath+"\archive") -ItemType Directory | Out-Null
+    }
+
+    If ((Test-Path -path $archivePath\$logYear) -eq $false) {
+        New-Item -Path $archivePath\$logYear -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path $archivePath\$logYear\$logMonth) -eq $false) {
+        New-Item -Path $archivePath\$logYear\$logMonth -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path $archivePath\$logYear\$logMonth\$logDate) -eq $false) {
+        New-Item -Path $archivePath\$logYear\$logMonth\$logDate -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path $archivePath\$logYear\$logMonth\$logDate\$ws1EnvUri) -eq $false) {
+        New-Item -Path $archivePath\$logYear\$logMonth\$logDate\$ws1EnvUri -ItemType Directory | Out-Null
+    }
+    <#If (!(Test-path -path $archivePath\$logYear\$logMonth\$logDate\$currentUser)) {
+        New-Item -Path $archivePath\$logYear\$logMonth\$logDate\$currentUser -ItemType Directory | Out-Null
+    }
+    #>
+    
+    $ws1InputArchive = (Get-Item $archivePath\$logYear\$logMonth\$logDate).FullName
+    return  $ws1InputArchive;
+}
+
+Function new-ws1InputArchive {
+    param(
+        [Parameter(Mandatory=$true,Position=0)][string]$logYear,
+        [Parameter(Mandatory=$true,Position=1)][string]$logMonth,
+        [Parameter(Mandatory=$true,Position=2)][string]$logDate
+    )
+    
+    $inputPath = "{0}\input" -f ((get-location).path)
+    $archivePath = "{0}\input\archive" -f ((get-location).path)
+
+    if (!(Test-path -path ($archivePath))) {
+        New-Item -path ($archivePath) -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -path $archivePath\$logYear) -eq $false) {
+        New-Item -Path $archivePath\$logYear -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path $archivePath\$logYear\$logMonth) -eq $false) {
+        New-Item -Path $archivePath\$logYear\$logMonth -ItemType Directory | Out-Null
+    }
+    If ((Test-Path -Path $archivePath\$logYear\$logMonth\$logDate) -eq $false) {
+        New-Item -Path $archivePath\$logYear\$logMonth\$logDate -ItemType Directory | Out-Null
+    }
+    [string]$archivePath = "{0}\{1}\{2}\{3}" -f $archivePath,$logYear,$logMonth,$logDate
+    return $archivePath
+}
+
+<###############################
+###
+###  PS DRIVE FUNCTIONS
+###
+###############################>
+
+
+
+###Create necessary subfolders if they don't already exist.
+function get-ws1Folders {
+$folders = @("code","config","documentation","input","output","tools","test")
+foreach ($folder in $folders) {
+    If (Test-Path .\$folder) {
+        write-host "$folder Found!"
+    }
+    else {
+        New-Item -ItemType Directory $folder
+    }
+}
 }
