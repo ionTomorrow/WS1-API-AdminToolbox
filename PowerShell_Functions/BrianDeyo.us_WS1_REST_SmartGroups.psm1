cc151c42ace9f570367c11c7e18f5aed6a388049
deyo0001 <brian.r.deyo@census.gov>
2021-01-14 14:49:48 -0500

    Update BrianDeyo.us_WS1_REST_SmartGroups.psm1
    Added new function to search for a Smart Group

--- PowerShell_Functions/BrianDeyo.us_WS1_REST_SmartGroups.psm1
+++ PowerShell_Functions/BrianDeyo.us_WS1_REST_SmartGroups.psm1
@@ -9,3 +9,39 @@ Function get-ws1SmartGroup {
 
     return $WS1CustomerOG
 }
+
+function find-ws1SmartGroup {
+    param (
+        [Parameter(Mandatory=$false,Position=0)][string]$name,
+        [Parameter(Mandatory=$false,Position=1)][int]$ogID,
+        [Parameter(Mandatory=$false,Position=2)][int]$managedByOgID,
+        [Parameter(Mandatory=$false,Position=3)][datetime]$modifiedFrom,
+        [Parameter(Mandatory=$false,Position=4)][datetime]$modifiedTill,
+        [Parameter(Mandatory=$false,Position=5)][string]$orderBy,
+        [Parameter(Mandatory=$false,Position=6)][ValidateSet("ASC","DESC")][string]$sortOrder, 
+        [Parameter(Mandatory=$false,Position=7)][int]$page,
+        [Parameter(Mandatory=$false,Position=8)][int]$pagesize,
+        [Parameter(Mandatory=$false,Position=10)][hashtable]$headers
+
+    )
+
+    $searchString = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+    if ($name) {$searchString.add('name',$name)}
+    if ($ogID) {$searchString.Add('organizationgroupid',$ogID)}
+    if ($managedByOgID) {$searchString.Add('managedbyorganizationgroupid',$managedByOgID)}
+    if ($modifiedFrom) {$searchstring.add('modifiedfrom',$modifiedFrom)}
+    if ($modifiedTill) {$searchstring.add('modifiedtill',$modifiedTill)}
+    if ($orderBy) {$searchString.Add('orderby',$orderBy)}
+    if ($sortOrder) {$searchString.Add('sortorder',$sortOrder)}
+    if ($page) {$searchString.Add('page',$page)}
+    if ($pagesize) {$searchString.Add('pagesize',$page)}
+
+    try {
+        $searchResults = Invoke-RestMethod -method GET -URI https://$($headers.ws1ApiUri)/api/mdm/smartgroups/search?$($searchString.ToString()) -Headers $headers
+    }
+    catch [Exception] {
+
+    }
+
+    return $searchResults
+}
